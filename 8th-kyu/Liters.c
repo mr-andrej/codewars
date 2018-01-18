026a9daa1d6e4b598470d63a9e098549ab627b0b
Andrej Ilievski <34969186+mr-andrej@users.noreply.github.com>
2018-01-18 12:48:45 +0100

    Add files via upload

---
+++ 8th-kyu/Liters.c
@@ -0,0 +1,7 @@
+// Written by mr-andrej
+// "Keep Hydrated!" by paulrichard
+// https://www.codewars.com/kata/582cb0224e56e068d800003c
+// Kyu: 8
+int Liters(double time) {
+  return ((int) (0.5 * time));
+}
