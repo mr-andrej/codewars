58678336b807a6d370c350efffe58c5e4b742127
Andrej Ilievski <34969186+mr-andrej@users.noreply.github.com>
2018-01-18 16:53:06 +0100

    Create solution.c

---
+++ 6th-kyu/solution.c
@@ -0,0 +1,9 @@
+// Written by mr-andrej
+// "Multiples of 3 or 5" by jhoffner
+// hhttps://www.codewars.com/kata/514b92a657cdc65150000006
+// Kyu: 6
+int solution(int number) {
+    int sum = 0;
+    for (int i = 0; i < number; sum += (i % 3 == 0 || i % 5 == 0) ? i : 0, i++);
+    return (sum);
+}
